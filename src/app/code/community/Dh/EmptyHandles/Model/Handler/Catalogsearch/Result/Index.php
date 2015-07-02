18da88cd93c781380adb65b6e0226822e1bbdb36
Geza Buza <gbuza@sessiondigital.com>
2015-07-02 18:34:27 +0200

    Fix catalog search result calculation
    The way how the module calculates the number of results is incorrect. When a layered navigation filter is active and the result is zero, then the module still thinks it has non-zero results, because only the search keyword is taken into account. This commit builds the navigation layer model properly by applying all active filters and finally counts the number of items in the result collection.

--- src/app/code/community/Dh/EmptyHandles/Model/Handler/Catalogsearch/Result/Index.php
+++ src/app/code/community/Dh/EmptyHandles/Model/Handler/Catalogsearch/Result/Index.php
@@ -19,10 +19,34 @@ class Dh_EmptyHandles_Model_Handler_Catalogsearch_Result_Index
      */
 	public function execute(Varien_Event $event)
 	{
-		$numResults = (bool)Mage::helper('catalogsearch')->getQuery()->getNumResults();
+		$numResults = $this->_getLayer()->getProductCollection()->getSize();
 		if (! $numResults) {
 			$this->_addHandle('catalogsearch_result_index_empty');
 		}
 		return $this;
 	}
+
+    /**
+     * Load the layer model with navigation layer filters applied on it
+     *
+     * @return Mage_Catalog_Model_Layer
+     */
+    protected function _getLayer()
+    {
+        $layer = Mage::registry('current_layer');
+        $factory = $this->_getFilterModelFactory();
+        foreach ($layer->getFilterableAttributes() as $attribute) {
+            $filterModel = $factory->create($attribute);
+            $filterModel
+                ->setLayer($layer)
+                ->setAttributeModel($attribute)
+                ->apply(Mage::app()->getRequest(), null);
+        }
+        return $layer;
+    }
+
+    protected function _getFilterModelFactory()
+    {
+        return Mage::getModel('emptyhandles/handler_catalogsearch_result_filterFactory');
+    }
 }
